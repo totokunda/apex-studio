fbc88e208bbdccdb00207bd18597caada0b9e926
ToToKu <totokunda@gmail.com>
2026-01-29 07:24:11 +0000

    Minor modification to text encoder:

--- apps/train/zimage/m.py
+++
@@ -1,6 +0,0 @@
-p = "/home/tosin_coverquick_co/apex-studio/apps/train/zimage/comfyui_adapter_model.safetensors"
-
-from safetensors.torch import load_file
-model = load_file(p)
-
-print(model.keys())
