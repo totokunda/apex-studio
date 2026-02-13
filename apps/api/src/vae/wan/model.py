1346d0f3217414924f4883b8db6edb86ab3d70b7
ToToKu <totokunda>
2026-02-13 12:40:31 +0000

    Fix i2v

--- apps/api/src/vae/wan/model.py
+++ apps/api/src/vae/wan/model.py
@@ -420,7 +420,7 @@ class WanAttentionBlock(nn.Module):
         q, k, v = qkv.chunk(3, dim=-1)
 
         # apply attention
-        x = attention_register.call(q, k, v)
+        x = attention_register.call(q, k, v, key="sdpa")
 
         x = x.squeeze(1).permute(0, 2, 1).reshape(batch_size * time, channels, height, width)
 
